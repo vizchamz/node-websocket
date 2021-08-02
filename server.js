72b274138404a2ceee7a243768ca160845e14f81
vizchamz <visal@xgengroup.com.au>
2021-08-02 13:27:27 +0530

    updated

--- server.js
+++ server.js
@@ -2,36 +2,30 @@ const http = require('http');
 const fs = require('fs');
 const webSocketServer = require('websocket').server;
 
-fs.readFile('client1.html', (err, html) => {
-    if (err) {
-        throw err;
-    }
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('content-type', 'text/html');
+    res.write(html);
+    res.end();
+});
 
-    const server = http.createServer ((req, res) => {
-        res.statusCode = 200;
-        res.setHeader ('content-type', 'text/html');
-        res.write(html);
-        res.end();
+const port = process.env.PORT || 9898;
+
+const wsServer = new webSocketServer({
+    httpServer: server
+});
+
+wsServer.on('request', function (request) {
+    const connection = request.accept(null, request.origin);
+    connection.on('message', function (message) {
+        console.log('Received Message:', message.utf8Data);
+        connection.sendUTF('Hi this is WebSocket server!');
     });
-
-    const port = process.env.PORT || 9898;
-
-    const wsServer = new webSocketServer({
-        httpServer: server
+    connection.on('close', function (reasonCode, description) {
+        console.log('Client has disconnected.');
     });
+});
 
-    wsServer.on('request', function (request) {
-        const connection = request.accept(null, request.origin);
-        connection.on('message', function (message) {
-            console.log('Received Message:', message.utf8Data);
-            connection.sendUTF('Hi this is WebSocket server!');
-        });
-        connection.on('close', function (reasonCode, description) {
-            console.log('Client has disconnected.');
-        });
-    });
-
-    server.listen(port, () => {
-        console.log('Server started on port ' + port);
-    });
+server.listen(port, () => {
+    console.log('Server started on port ' + port);
 });
